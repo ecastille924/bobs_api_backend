18c8a13f1138953f6d940665a9405f3f0db5dcd2
Erick Castille <ecastille924@gmail.com>
2021-04-28 18:40:33 -0400

    building create action in quotes controller

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -8,4 +8,13 @@ class QuotesController < ApplicationController
         quote = Quote.find(params[:id])
         render json:QuoteSerializer.new(quote)
     end
+
+    def create 
+        quote = Quote.new(quote_params)
+        if quote.save
+            render json: QuoteSerializer.new(quote)
+        else 
+            render json: {error: "quote invalid"}
+        end
+    end
 end
